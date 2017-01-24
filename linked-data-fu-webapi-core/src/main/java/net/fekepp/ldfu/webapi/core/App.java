1445a85e65bf9b9c84c18b679af66efa98efe6d7
Felix Leif Keppmann <felix.leif@keppmann.de>
2017-01-24 16:26:29 +0100

    Added project stub

---
+++ linked-data-fu-webapi-core/src/main/java/net/fekepp/ldfu/webapi/core/App.java
@@ -0,0 +1,16 @@
+package net.fekepp.ldfu.webapi.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+	private static final Logger loggerStatic = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+
+		loggerStatic.info("public static void main(String[] args)");
+
+	}
+
+}
