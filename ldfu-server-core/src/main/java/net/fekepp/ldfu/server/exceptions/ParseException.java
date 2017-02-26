b43e42f5d982550011703f4209080daf371b9bf8
Felix Leif Keppmann <felix.leif@keppmann.de>
2017-02-26 23:48:54 +0100

    Added new exceptions used during parsing

---
+++ ldfu-server-core/src/main/java/net/fekepp/ldfu/server/exceptions/ParseException.java
@@ -0,0 +1,7 @@
+package net.fekepp.ldfu.server.exceptions;
+
+public class ParseException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ ldfu-server-core/src/main/java/net/fekepp/ldfu/server/exceptions/ParserException.java
@@ -0,0 +1,7 @@
+package net.fekepp.ldfu.server.exceptions;
+
+public class ParserException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
