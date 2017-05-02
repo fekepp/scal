dff567b388db1113c27d97b06bb0d2c896b11fa9
Felix Leif Keppmann <felix.leif@keppmann.de>
2017-05-02 21:57:21 +0200

    Added table model with default TSV and CSV formats

---
+++ ldfu-server-core/src/main/java/net/fekepp/ldp/model/TableModel.java
@@ -0,0 +1,84 @@
+package net.fekepp.ldp.model;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import jersey.repackaged.com.google.common.collect.Sets;
+import net.fekepp.ldp.Format;
+import net.fekepp.ldp.FormatConverter;
+import net.fekepp.ldp.Model;
+import net.fekepp.ldp.ModelConverter;
+import net.fekepp.ldp.format.CsvFormat;
+import net.fekepp.ldp.format.TsvFormat;
+
+public class TableModel implements Model {
+
+	private static String NAME = "Table";
+
+	private static Format DEFAULT_FORMAT = TsvFormat.getInstance();
+
+	private static Set<Format> FORMATS = Sets.newHashSet(DEFAULT_FORMAT, CsvFormat.getInstance());
+
+	private static class InstanceHolder {
+		static final TableModel INSTANCE = new TableModel();
+	}
+
+	private TableModel() {
+	}
+
+	public static TableModel getInstance() {
+		return InstanceHolder.INSTANCE;
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public Format getDefaultFormat() {
+		return DEFAULT_FORMAT;
+	}
+
+	@Override
+	public Set<Format> getFormats() {
+		return Collections.unmodifiableSet(FORMATS);
+	}
+
+	@Override
+	public Map<String, Format> getMediaTypesMap() {
+		Map<String, Format> contentTypesMap = new HashMap<String, Format>();
+		for (Format format : FORMATS) {
+			for (String mediaType : format.getMediaTypes()) {
+				contentTypesMap.put(mediaType, format);
+			}
+		}
+		return contentTypesMap;
+	}
+
+	@Override
+	public Map<String, Format> getFileExtensionsMap() {
+		Map<String, Format> fileExtensionsMap = new HashMap<String, Format>();
+		for (Format format : FORMATS) {
+			for (String fileExtensions : format.getFileExtensions()) {
+				fileExtensionsMap.put(fileExtensions, format);
+			}
+		}
+		return fileExtensionsMap;
+	}
+
+	@Override
+	public ModelConverter buildModelConverter(Model sinkFormatGroup) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FormatConverter buildFormatConverter(Format sourceFormat, Format sinkFormat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
